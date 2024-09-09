f3d57b6191d52c7f01cc176311b6e5ed1d027a13
ekaterinamavliutova <akumaaya@gmail.com>
2024-09-09 17:38:43 +0200

    update jest config

--- jest.config.mjs
+++ jest.config.mjs
@@ -4,7 +4,6 @@ const config = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./jest.setup.js'],
   moduleNameMapper: {
-    "^.+\\.svg$": "jest-svg-transformer",
     "^.+\\.(css|less|scss)$": "identity-obj-proxy",
   }
 }
