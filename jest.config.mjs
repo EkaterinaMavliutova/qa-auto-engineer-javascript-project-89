01039b1afec709f470cf7d9a7ece9d686c21ded6
ekaterinamavliutova <akumaaya@gmail.com>
2024-09-12 14:51:01 +0200

    add react component to test widget integration

--- jest.config.mjs
+++ jest.config.mjs
@@ -5,7 +5,9 @@ const config = {
   setupFilesAfterEnv: ['./jest.setup.js'],
   moduleNameMapper: {
     "^.+\\.(css|less|scss)$": "identity-obj-proxy",
-  }
+  },
+  extensionsToTreatAsEsm: ['.ts', '.jsx', '.tsx'],
+  modulePathIgnorePatterns: ['<rootDir>/__tests__/utils.js'],
 }
 
 export default config;
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,9 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
-import '@hexlet/chatbot-v2/styles';
 import ReactDOM from 'react-dom/client';
-import Widget from '@hexlet/chatbot-v2';
-import steps from '@hexlet/chatbot-v2/example-steps';
-import '@hexlet/chatbot-v2/styles';
+// import Widget from '@hexlet/chatbot-v2';
+// import steps from '@hexlet/chatbot-v2/example-steps';
+// import '@hexlet/chatbot-v2/styles';
+import App from './App';
 
 const container = document.getElementById('root');
 ReactDOM.createRoot(container)
-  .render(Widget(steps));
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
+  .render(<App/>);
